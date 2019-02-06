583ce99bb493e2b0aa06c99226d117b21d3828b7
todor996 <todor_1996@live.com>
2019-02-06 21:39:31 +0100

    node i express podesavanja

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+require('./api/models/db');
+require('./api/config/passport');
+
+var routesApi = require('./api/routes/index');
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.use(passport.initialize());
+app.use('/api', routesApi);
+
+app.use((err, req, res, next) =>{
+    if (err.name === 'UnauthorizedError') {
+      res.status(401);
+      res.json({"message" : err.name + ": " + err.message});
+    }
+  });
